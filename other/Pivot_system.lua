05aeb8bb52989f47cf3907e16363bba44199ca6d
eortigoza5 <36522837+eortigoza5@users.noreply.github.com>
2021-11-21 10:40:42 -0300

    Update Pivot_system.lua

--- other/Pivot_system.lua
+++ other/Pivot_system.lua
@@ -5,7 +5,7 @@ instrument {overlay = true,
 pivbuy =low[1] < low[2] and low > low[1] and high[1] < high[2] and high > high[1] and close > close[2]
 pivsell =high[1] > high[2] and high < high[1] and low[1] > low[2] and low < low[1] and close < close[2]
 
-plot_shape(pivbuy, "BUY", shape_style.arrowup, shape_size.large, "green", shape_location.belowbar, 0, "buy","green" )
+plot_shape(pivbuy, "BUY", shape_style.arrowup, shape_size.large, "lime", shape_location.belowbar, 0, "buy","green" )
 plot_shape(pivsell, "SELL", shape_style.arrowdown, shape_size.large, "red", shape_location.abovebar, 0, "Sell", "red" )
 
 
