d5cddb12b22936836a11cbdf4e7a2d8be61ef682
Alina Rastorgueva <alina.rastorgueva@quadcode.com>
2025-03-13 14:25:40 +0200

    Fix ALMA parameters' names

--- averages/alma.lua
+++ averages/alma.lua
@@ -1,14 +1,14 @@
 instrument { name = "ALMA", overlay = true }
 
-period = input (9,    "front.period", input.integer, 1,  100   )
-offset = input (0.85, "front.newind.offset", input.double,  0, 100, 0.01)
-sigma  = input (6,    "front.newind.alma.sigma",  input.double,  0.01, 10, 1)
-source = input (1,    "front.ind.source", input.string_selection, inputs.titles_overlay)
+period = input (9, "Period", input.integer, 1,  100)
+offset = input (0.85, "Offset", input.double,  0, 100, 0.01)
+sigma  = input (6, "Alma Sigma",  input.double,  0.01, 10, 1)
+source = input (1, "Source", input.string_selection, inputs.titles_overlay)
 
 input_group {
-    "front.ind.dpo.generalline",
+    "Alma Line",
     color = input { default = "#56CEFF", type = input.color },
-    width = input { default = 1, type = input.line_width}
+    width = input { default = 1, type = input.line_width }
 }
 
 local sourceSeries = inputs [source]
